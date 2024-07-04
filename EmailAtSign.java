6c186bc604d36697520fdbffec5cdb712f18f985
glenndsilva1 <glenndsilva@rediffmail.com>
2024-07-04 16:43:40 +0530

    EmailAtSign.java

--- EmailAtSign.java
+++ EmailAtSign.java
@@ -1,12 +1,20 @@
 package com.servlet.cor.atm.validation;
 
-public class EmailAtSign extends Validations{
-	@Override
+public class EmailAtSign extends Validations {
+	public EmailAtSign() {
+		super();
+	}
+
 	public void apply(String str) {
 		if(str != null){
-			if(!str.equals("@")) {
-				System.out.printf("Email Does Not Have At Sign");
+			if(!str.contains("@")){
+				System.out.println("Email Does Not Have At Sign");
 			}
+		}
+		if(nextDispensor != null){
+			nextDispensor.apply(str);
+		}else {
+			System.out.println("Stop Validations Over");
 		}
 	}
 }
