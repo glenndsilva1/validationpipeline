998f147100cfac87ee20cbda7752ee17914cb94c
glenndsilva1 <glenndsilva@rediffmail.com>
2024-07-04 16:44:20 +0530

    EmailDotSign.java

--- EmailDotSign.java
+++ EmailDotSign.java
@@ -1,13 +1,21 @@
 package com.servlet.cor.atm.validation;
 
 public class EmailDotSign extends Validations {
+	EmailDotSign() {
+		super();
+	}
+
 	@Override
 	public void apply(String str) {
-		if(str != null){
-			if(!str.equals(".")) {
-				System.out.printf("Email Does Not Have Dot Sign");
+		if (str != null) {
+			if (!str.contains(".")) {
+				System.out.println("Email Does Not Have Dot Sign");
 			}
+		}
+		if(nextDispensor!=null) {
+			nextDispensor.apply(str);
+		}else {
+			System.out.println("Stop Validations Over");
 		}
 	}
 }
-
